78616ff0fd9130d7eda16123e54f3ccfd213d7f6
EricTrainingRev <eric.suminski@revature.com>
2025-06-24 12:14:26 -0400

    merging jobs into one

--- .github/scripts/pull-request.sh
+++ .github/scripts/pull-request.sh
@@ -5,11 +5,15 @@ existing_pr=$(gh pr list --base main --head "$GITHUB_REF_NAME" --json number --j
 
 if [ -z "$existing_pr" ]; then
     echo "No existing PR found. Creating a new one..."
-    gh pr create \
+    pr_url=$(gh pr create \
         --base main \
         --head "$GITHUB_REF_NAME" \
         --title "Auto PR: $GITHUB_REF_NAME to main" \
-        --body "This pull request was automatically created after successful tests."
+        --body "This pull request was automatically created after successful tests." \
+        --json number --jq '.number')
+    echo "Created PR #$pr_url"
+    echo "pull_request_number=$pr_url" >> "$GITHUB_OUTPUT"
 else
     echo "PR already exists: #$existing_pr"
+    echo "pull_request_number=$existing_pr" >> "$GITHUB_OUTPUT"
 fi
